67bac0a26721b919d68f903ab5a9a6e729a7d649
Lana201999 <lanasharabati99@gmail.com>
2021-09-21 12:50:18 +0300

    .
    .

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import { useEffect , useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import loding from './loding';
+
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+function App() {
+  const [post,setPost] = useState([]);
+  const [loding,setLoding] = useState(false);
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => {
+    setPost(res.data)
+  setLoding(true)})
+  })
+ 
+const styles2={
+  display: " inline-block",
+  margin:"50px",
+  padding:"20px",
+  width: "18rem"
+}
+  return (
+    <div>
+    {loding? post.map((item) =>
+    <Card sx={{ minWidth: 275 }} key={item.id} style={styles2}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Title
+        </Typography>
+        <Typography variant="h5" component="div" >
+         {item.title}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          body
+        </Typography>
+        <Typography variant="body2">
+          {item.body}
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>):<loding/>}
+   </div>
+  );
+}
+
+export default App;
+// <div className="App">
+    //   <ul>
+    //     {
+    //       post.map(p =><li key={p.id}>{p.title}</li>)
+    //     }
+    //   </ul>
+      
+    // </div>
---
+++ src/loding.js
@@ -0,0 +1,7 @@
+import React from "react";
+import './App.css';
+function loding(props) {
+
+    return(<div class="lds-ring"><div></div><div></div><div></div><div></div></div>);
+}
+export default loding;
